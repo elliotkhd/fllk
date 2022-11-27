7f8cb6a1c4c460bf92d2443f0ec62fa3bd5e93d3
elliotkhd <elliotkhd@gmail.com>
2022-11-27 14:45:37 +0800

    build from latest code base

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "c1e17d5e887e9b75b2a48b8291a98d34",
-"index.html": "05f9133eb6722f8a699f32a15208e804",
-"/": "05f9133eb6722f8a699f32a15208e804",
-"main.dart.js": "eaa97299f0223e6563e1552bb1a93907",
+"index.html": "e22fca66cfcca59e607a36d8d7ac918c",
+"/": "e22fca66cfcca59e607a36d8d7ac918c",
+"main.dart.js": "6d41c8d45da5e0437047a667611d83a2",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
